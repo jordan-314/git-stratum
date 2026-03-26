9e624859b8583019af5b6403c363fd528b184ad9
Jordan <165378667+jordan-314@users.noreply.github.com>
2026-03-26 19:54:02 +0000

    feat: Define method for handling repository paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 // For now include everything as pub mod to get errors in IDE.
+use git_url_parse::GitUrlParseError;
 use thiserror::Error;
 
 pub mod actor;
 pub mod commit;
+pub mod remote;
 pub mod repository;
 
 pub use actor::MinedActor;
@@ -14,6 +16,14 @@ pub enum StratumError {
     /// An abstraction of git2::Error to raise the error effectively
     #[error("{0}")]
     GitError(git2::Error),
+
+    /// An abstraction of git-url-parse::GitUrlParseError
+    #[error("{0}")]
+    GitUrlError(GitUrlParseError),
+
+    /// If a URL can be parsed but is not a valid GitUrl schem
+    #[error("Invalid scheme: {0}")]
+    GitUrlSchemeError(String),
 
     /// An error associated with a bad path
     #[error("{0}")]
